4b20eeaa3a3fdf85350b4aedf697fd418eb91249
Christoph Stäbler <cstabler@redhat.com>
2024-06-27 09:37:12 +0200

    Revert "Enable debug logs in generate.sh (#647)"
    This reverts commit bc894043992edc8b3ae778b2eda12cd3583964f7.

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
@@ -15,5 +15,3 @@ $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
   --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.16"
-
-set +x

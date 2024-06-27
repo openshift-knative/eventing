361c01da11bb75c9788069daac7c364a4b6b7c6a
Christoph Stäbler <cstabler@redhat.com>
2024-06-27 09:04:20 +0200

    Use go install

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -8,10 +8,10 @@ tmp_dir=$(mktemp -d)
 git clone --branch main https://github.com/openshift-knative/hack "$tmp_dir"
 
 pushd "$tmp_dir"
-go build -o "$tmp_dir"/generate cmd/generate/generate.go
+go install github.com/openshift-knative/hack/cmd/generate
 popd
 
-"$tmp_dir"/generate \
+$(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
   --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.16"

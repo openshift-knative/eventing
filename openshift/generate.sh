f6e62d6c37a1816e2a9cc56a2155d02f58c5422f
Christoph Stäbler <cstabler@redhat.com>
2024-10-30 08:23:22 +0100

    Install tzdata in Dockerfiles

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -16,4 +16,5 @@ rm -rf "$tmp_dir"
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
+  --additional-packages tzdata \
   --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.17"

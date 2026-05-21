3bedc2cbcf23f4b0e18318d7cee153253e9db30d
Christoph Stäbler <cstabler@redhat.com>
2026-05-21 14:35:09 +0200

    Add patch for metric attributes deny list
    Ensures the vendor and configmap changes survive make generate-release.

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -13,6 +13,7 @@ echo "Release: $release"
 
 "${root_dir}"/hack/update-codegen.sh
 git apply "${root_dir}"/openshift/patches/027-rekt-serviceaccounts-delete.patch
+git apply "${root_dir}"/openshift/patches/028-metrics-attributes-deny.patch
 
 ./openshift/generate.sh
 
